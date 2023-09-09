b1366d160890200781bc17a3af015f2126f0b179
DrafateYassine <y.drafate.intern@get-consult.com>
2023-09-09 14:39:20 +0100

    login template

--- home/views.py
+++ home/views.py
@@ -1,10 +1,8 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
-
+from django.urls import reverse
 
 
 def home(request):
     return render(request, 'home/home.html')
 
-
-def register(request):
-    return render(request, 'registration/register.html')
